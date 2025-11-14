69690446f858a42f03141c654aad168884ebbfbd
Tailane Isaias <tailaneisaias08@gmail.com>
2025-11-14 14:53:56 -0300

    Nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -53,6 +53,60 @@ int main()
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
+#define TAM 10
+#define NAVIO 3
+    int tabuleiro[TAM][TAM];
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tabuleiro[i][j] = 0;
+
+    int navio1[NAVIO] = {3, 3, 3};
+    int navio2[NAVIO] = {3, 3, 3};
+    int navio3[NAVIO] = {3, 3, 3};
+    int navio4[NAVIO] = {3, 3, 3};
+
+    int l1 = 2, c1 = 1;
+    int l2 = 5, c2 = 7;
+    int l3 = 0, c3 = 0;
+    int l4 = 0, c4 = 9;
+
+    if (c1 + NAVIO > TAM)
+        return 1;
+    if (l2 + NAVIO > TAM)
+        return 1;
+    if (l3 + NAVIO > TAM || c3 + NAVIO > TAM)
+        return 1;
+    if (l4 + NAVIO > TAM || c4 - (NAVIO - 1) < 0)
+        return 1;
+
+    for (int i = 0; i < NAVIO; i++)
+    {
+        if (tabuleiro[l1][c1 + i] == 3)
+            return 1;
+        if (tabuleiro[l2 + i][c2] == 3)
+            return 1;
+        if (tabuleiro[l3 + i][c3 + i] == 3)
+            return 1;
+        if (tabuleiro[l4 + i][c4 - i] == 3)
+            return 1;
+    }
+
+    for (int i = 0; i < NAVIO; i++)
+    {
+        tabuleiro[l1][c1 + i] = navio1[i];
+        tabuleiro[l2 + i][c2] = navio2[i];
+        tabuleiro[l3 + i][c3 + i] = navio3[i];
+        tabuleiro[l4 + i][c4 - i] = navio4[i];
+    }
+
+    printf("Tabuleiro 10x10:\n");
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+            printf("%d ", tabuleiro[i][j]);
+        printf("\n");
+    }
+
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
